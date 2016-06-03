5ea67dfaac6029c94d4f8446a26e211ba402cc0c
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-02 17:30:37 -0700

    add video selection callback

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBqurhP4BNtf2u1hmvbvrC3w1DHuAvUTP8'; //Youtube API Key
 
@@ -11,10 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
 
   }
@@ -22,7 +29,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={ this.state.videos }/>
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+          videos={ this.state.videos }/>
       </div>
     );
   }
